bf479abbc3765c3e55206ba5dbf19c6e01f34161
Thomas Mundal <thmundal@gmail.com>
2017-06-02 13:24:17 +0200

    Dynamic protocol

--- engine.js
+++ engine.js
@@ -8,8 +8,8 @@ if(isUnSupported()) {
 }
 var callbacks = [];
 function l() {
-	var css = ["http:\/\/fonts.googleapis.com/css?family=Open+Sans+Condensed:300",
-               "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"].concat(engine.css);;
+	var css = ["\/\/fonts.googleapis.com/css?family=Open+Sans+Condensed:300",
+               "\/\/maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"].concat(engine.css);;
 	// DO NOT LOAD THIS FILE (default.js) IN THE ARRAY! MEGASUPERRECURSIONMONSTER!
 	var js = engine.js;
 
@@ -17,7 +17,7 @@ function l() {
         throw new Error("Fatal error: engine.js included in load will cause an endless loop.");
     }
 
-    var depends = ["http:\/\/code.jquery.com/jquery-3.2.1.min.js", "http:\/\/code.jquery.com/ui/1.12.1/jquery-ui.min.js"];
+    var depends = ["\/\/code.jquery.com/jquery-3.2.1.min.js", "\/\/code.jquery.com/ui/1.12.1/jquery-ui.min.js"];
     var dcount = 0;
 
     function dependcomplete() {
